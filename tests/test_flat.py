5c131b04daea887ddeb4c9ab6f59cad8e6b0da8e
maksimov.yd <maksimov.yd@gmail.com>
2023-09-17 03:06:16 +0300

    add test no two

--- tests/test_flat.py
+++ tests/test_flat.py
@@ -6,4 +6,21 @@ def test_flat_gendiff():
     current_dir = os.path.dirname(__file__)
     file_path1 = os.path.join(current_dir, 'fixtures', 'file1.json')
     file_path2 = os.path.join(current_dir, 'fixtures', 'file2.json')
-    assert gendiff.generate_diff(file_path1, file_path2) == 'gendiff file1.json file2.json\n{\n  - follow: False\n    host: hexlet.io\n  - proxy: 123.234.53.22\n  - timeout: 50\n  + timeout: 20\n  + verbose: True\n}'
+    file_res = os.path.join(current_dir, 'fixtures', 'res_test_1_2.txt')
+    with open(file_res, 'r') as res_test_1_2:
+        res_file_1_2 = res_test_1_2.read()
+    assert gendiff.generate_diff(file_path1, file_path2) == str(res_file_1_2)
+
+
+def test_flat_gendiff_two_empty_file_two():
+    current_dir = os.path.dirname(__file__)
+    file_path1 = os.path.join(current_dir, 'fixtures', 'file_test_flat2_1.json')
+    file_path2 = os.path.join(current_dir, 'fixtures', 'file_test_flat2_2.json')
+    file_res = os.path.join(current_dir, 'fixtures', 'res_test_flat_2.txt')
+    with open(file_res, 'r') as res_test_1_2:
+        res_file_1_2 = res_test_1_2.read()
+    assert gendiff.generate_diff(file_path1, file_path2) == str(res_file_1_2)
+
+
+# Ожидаемое значение в тестах — объемная строчка. А в будущих шагах она станет значительно сложнее. В таких случаях принято сохранять их также в фикстуры в виде текстовых файлов и читать в нужных тестах
+# Убрать строку сравнение в файл

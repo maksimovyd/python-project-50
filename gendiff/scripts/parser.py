e178c53c485c622ccd9583a336738d9b7baf53d1
maksimov.yd <maksimov.yd@gmail.com>
2023-09-21 20:24:46 +0300

    final

--- gendiff/scripts/parser.py
+++ gendiff/scripts/parser.py
@@ -1,55 +1,6 @@
 import json
 import os
 import yaml
-from gendiff.scripts.stylish import stylish
-from gendiff.scripts.plain import plain
-
-
-def generate_diff(file_path1, file_path2, formatter):
-    dict_one = read_files_to_dict(file_path1)
-    dict_two = read_files_to_dict(file_path2)
-    res_dict = create_diff(dict_one, dict_two)
-    if formatter == 'stylish':
-        res_str = stylish(res_dict)
-        res_str = (
-            'gendiff ' + str(os.path.basename(file_path1)) +
-            ' ' + str(os.path.basename(file_path2)) + '\n' + res_str
-            )
-        print(res_str)
-        return res_str
-    elif formatter == 'plain':
-        res_str = plain(res_dict, dict_one, dict_two, [], '')
-        res_str = (
-            'gendiff --format plain ' + str(os.path.basename(file_path1)) +
-            ' ' + str(os.path.basename(file_path2)) + '\n' + res_str
-            )
-        res_str = res_str[:-1]
-        print(res_str)
-        return res_str
-    elif formatter == 'json':
-        print(json.dumps(res_dict))
-        return json.dumps(res_dict)
-
-
-def create_diff(dict_one, dict_two):
-    all_keys = sorted(list(dict_one.keys() | dict_two.keys()))
-    res_dict = {}
-    for key_ in all_keys:
-        if key_ in dict_one and key_ in dict_two:
-            if isinstance(dict_one[key_], dict) and \
-              isinstance(dict_two[key_], dict):
-                res_dict[('    ' +
-                          key_)] = create_diff(dict_one[key_], dict_two[key_])
-            elif dict_one[key_] == dict_two[key_]:
-                res_dict[('    ' + key_)] = dict_one[key_]
-            else:
-                res_dict[('  - ' + key_)] = dict_one[key_]
-                res_dict[('  + ' + key_)] = dict_two[key_]
-        elif key_ in dict_one and key_ not in dict_two:
-            res_dict[('  - ' + key_)] = dict_one[key_]
-        else:
-            res_dict[('  + ' + key_)] = dict_two[key_]
-    return res_dict
 
 
 def read_files_to_dict(file_path):

18e0aff28471bd1131ec834371084e2fb069c1dd
maksimov.yd <maksimov.yd@gmail.com>
2023-09-21 18:32:37 +0300

    add tests plain yml and json

--- tests/test_flat.py
+++ tests/test_flat.py
@@ -52,5 +52,25 @@ def test_two_level_json():
     assert parser.generate_diff(file_path1, file_path2, 'stylish') == str(res_file_1_2)
 
 
+def test_two_level_plain_yml():
+    current_dir = os.path.dirname(__file__)
+    file_path1 = os.path.join(current_dir, 'fixtures', 'yml_file_1_two_level.yml')
+    file_path2 = os.path.join(current_dir, 'fixtures', 'yml_file_2_two_level.yml')
+    file_res = os.path.join(current_dir, 'fixtures', 'res_plain_yml.txt')
+    with open(file_res, 'r') as res_test_1_2:
+        res_file_1_2 = res_test_1_2.read()
+    assert parser.generate_diff(file_path1, file_path2, 'plain') == str(res_file_1_2)
+
+
+def test_two_level_plain_json():
+    current_dir = os.path.dirname(__file__)
+    file_path1 = os.path.join(current_dir, 'fixtures', 'file_1_two_level.json')
+    file_path2 = os.path.join(current_dir, 'fixtures', 'file_2_two_level.json')
+    file_res = os.path.join(current_dir, 'fixtures', 'res_plain_json.txt')
+    with open(file_res, 'r') as res_test_1_2:
+        res_file_1_2 = res_test_1_2.read()
+    assert parser.generate_diff(file_path1, file_path2, 'plain') == str(res_file_1_2)
+
+
 # Ожидаемое значение в тестах — объемная строчка. А в будущих шагах она станет значительно сложнее. В таких случаях принято сохранять их также в фикстуры в виде текстовых файлов и читать в нужных тестах
 # Убрать строку сравнение в файл
